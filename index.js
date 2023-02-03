f40b7383202db1b8cfd6f73a9cc3b36a56153bde
jobemcamera <jobecamera@gmail.com>
2023-02-03 15:39:39 -0300

    criada função sacar dentro da classe ContaCorrente

--- index.js
+++ index.js
@@ -2,24 +2,41 @@
 class Cliente {
     nome;
     cpf;
-    agencia;
-    saldo;
 }
 
-// Criando um novo Cliente
-const cliente1 = new Cliente();
-const cliente2 = new Cliente();
+class ContaCorrente {
+    agencia;
+    saldo;
 
-// Atribuição dos valores nas propriedades da Classe
+    sacar(valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+        }
+    }
+}
+
+
+
+// Criando um novo Objeto / instanciando o Objeto
+const cliente1 = new Cliente();
+// Atribuição dos valores nas propriedades do Objeto
 cliente1.nome = 'Jobe';
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
+const cliente2 = new Cliente();
 cliente2.nome = 'Ana';
 cliente2.cpf = 22211133309;
-cliente2.agencia = 1002;
-cliente2.saldo = 20;
+
+const ContaCorrenteJobe = new ContaCorrente();
+ContaCorrenteJobe.saldo = 0;
+ContaCorrenteJobe.agencia = 1001;
+
+console.log(ContaCorrenteJobe.saldo);
+
+// Depósito 100
+ContaCorrenteJobe.saldo = 100;
+console.log(ContaCorrenteJobe.saldo);
+ContaCorrenteJobe.sacar(50);
 
 console.log(cliente1);
 console.log(cliente2);
